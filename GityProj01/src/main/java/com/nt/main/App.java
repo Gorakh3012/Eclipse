e4af71afc1c07d04517eb7654635d93999ca1b27
Gorakh3012 <dhaigudegorakh@gmail.com>
2026-06-07 12:24:00 +0530

    add method by dev2

--- GityProj01/src/main/java/com/nt/main/App.java
+++ GityProj01/src/main/java/com/nt/main/App.java
@@ -4,7 +4,16 @@ package com.nt.main;
  * Hello world!
  */
 public class App {
+	
+	public int add() {
+		int a = 0; int b = 0;
+		int c;
+		c= a+b;
+		return c;
+	}
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        App ap = new App();
+        ap.add();
     }
 }
